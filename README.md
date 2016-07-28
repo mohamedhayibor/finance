b8a41a282ce75c592737b0aa01a806d4be0459e8
Mohamed Hayibor <mohamedhayibor@gmail.com>
2016-07-28 12:44:33 -0400

    clean up and comment out irr to implement later

--- README.md
+++ README.md
@@ -1,35 +1,37 @@
-Finance
--------------
+# Finance
 
-This crate provides utility functions for the most common operations in Finance.
+This crate (Rust library) provides utility functions for the most common operations (calculations) in Finance.
 
 ## Usage
 
 Include this library with the following:
-```
+```rust
 [dependencies]
-finance = "0.0.0"
+finance = "0.0.0" // preferrably the latest version on crates.io
 ```
 
 ## API 
 
-1. present_value
-2. future_value
-3. irr
+1. present_value (rate: f64, compounding_periods: f64, future_value: f64) -> f64
+2. future_value (rate: f64, compounding_periods: f64, present_value: f64) -> f64
+3. internal_rate_of_return
 4. payback period
 5. ROI return_on_investment
 6. amortization
 7. discount factor
 8. compound_interest
-9. compound_annual_growth_rate
-10. leverage_ratio
-11. rule_of_72
-12. weighted_cost_of_capital
-13. pmt
+9. compound_annual_growth_rate (beginning_value: f64, ending_value: f64, number_of_periods: f64) -> f64
+10. leverage_ratio (total_liabilities: f64, total_debts: f64, total_income: f64) -> f64
+11. rule_of_72 (rate: f64) -> f64
+12. rule_of_70 (rate: f64) -> f64
+12. weighted_cost_of_capital (market_value_of_equity: f64, market_value_of_debt: f64, cost_of_equity: f64, cost_of_debt: f64, tax_rate: f64) -> f64
+13. PMT (rate: f64, number_of_payments: f64, principal: f64) -> f64
 
 > Unless specified the inputs are expected to be floats. Your program will `panic` if you pass integers.
 
-> Also rates are annualized by default. Input accordingly if calculating in (monthly, semi-annual, daily) terms.
+Also rates are annualized by default. Input accordingly if calculating in (monthly, semi-annual, daily) terms. In another words, if you make sure that your compounding_periods (daily, monthly, annual...) is used with corresponding rates (daily, monthly, annual).
+
+> For consistency sake `rate` is getting passed as a plain not float and not as a percentage (%)
 
 If you are wondering about compounding calculation. I got you [here](https://github.com/mohamedhayibor/rust_compound)
 
@@ -38,6 +40,8 @@ If you are wondering about compounding calculation. I got you [here](https://git
 Well save yourself some googling or fiddling wiki pages. All formulas have test cases and got battle tested before release. Feel free to send a PR if you feel like a missing formula should be included (preferably open an issue first :sunglasses:)
 
 > Do not worry about the functions taking owernership of anything. As primitives support the copy trait.
+
+Also some of the function names can be judged as quiet verbose and it is worthit. Abbreviations that you have no idea, about is the last thing you would want. (There aren't many programmers with a Finance background anyways)
 
 ## License
 
--- src/lib.rs
+++ src/lib.rs
@@ -56,16 +56,33 @@ fn test_net_present_value() {
 }
 */
 
-
+/*  Will implement IRR > later - haven't found the most effective way
 //  Internal Rate of Return (IRR) implementation
+pub fn internal_rate_of_return() {
+    unimplemented!();
+}
 
+#[test]
+fn test_irr() {
+    let test_value = internal_rate_of_return();
+    assert_eq!(test_value, 1000.);
+}
+*/
 
 
 //  Payback Period (PP) implementation
+//  How long will it take you to get the money back from your investment
+//  cfs means cash flows > streams of cash :)
+pub fn payback_period(number_of_periods: f64, cfs: &[f64]) -> f64 {
+    
+}
 
+#[test]
+fn test_PP() {
+    let test_value = payback_period(3., &[-1000.00, 300.00, 500.00, 500.00]);
 
-
-
+    assert_eq!(test_value, 3.);
+}
 
 //  Return On Investment (ROI) implementation
 
@@ -112,4 +129,56 @@ fn test_leverage_ratio() {
 }
 
 //  Weighted Cost of capital (WACC)
+//  Be aware that decimal formats are expected to be passed and not percentages.
+pub fn weighted_cost_of_capital(market_value_of_equity: f64, market_value_of_debt: f64, cost_of_equity: f64, cost_of_debt: f64, tax_rate: f64) -> f64 {
+    // champion of verbosity, lets abrreviate
+    let e = market_value_of_equity;
+    let d = market_value_of_debt;
+    // v: market value of the entity, no worries it is not taking owership as primitives have the copy trait implemented
+    let v = e + d;
+    let re = cost_of_equity;
+    let rd = cost_of_debt;
+    let t = tax_rate;
 
+    let wacc = ((e / v) * re ) + (((d / v) * rd ) * (1. - t));
+    round(wacc, 4)
+}
+
+#[test]
+fn test_wacc() {
+    // implement an example later
+    let test_value = weighted_cost_of_capital(2000000.00, 1000000.00, 0.07, 0.05, 0.4);
+    assert_eq!(test_value, 0.0567);
+}
+
+// PMT: amount of loan to pay assuming a constant interest rate
+// a negative value represent a negative cash flow in Finance, meaning that you have to pay out that particular amount (in another words, money from your pockets, going into someonelse
+pub fn PMT(rate: f64, number_of_payments: f64, principal: f64) -> f64 {
+    let discount_factor = 1. + rate;
+
+    round( -principal * rate / ( 1. - discount_factor.powf(-number_of_payments) ), 2)
+}
+
+#[test]
+fn test_PMT() {
+    // lets consider the case where you are getting a $50,000.00 laon for 30 years at the rate of 5%
+    let test_value = PMT(0.05, 360., 50000.00);
+
+    assert_eq!(test_value, 3000.);
+}
+
+// Compound Annual Growth Rate: CAGR
+pub fn compound_annual_rate(beginning_value: f64, ending_value: f64, number_of_periods: f64) -> f64 {
+    let delta = ending_value / beginning_value;
+
+    let CAGR = delta.powf( 1. / number_of_periods) - 1.;
+    // Alert highly double check the formula
+    round(CAGR, 2)
+}
+
+#[test]
+fn test_CAGR() {
+    let test_value = compound_annual_rate(20000.00, 15000.00, 3.);
+    // PLEASE double check formula before
+    assert_eq!(test_value, 12.88);
+}
